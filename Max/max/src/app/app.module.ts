e45f532507797ebf2e33916883f23a61128f6dd3
John Muiruri <themasalacollective@gmail.com>
2022-04-07 09:23:37 +0300

    Added a warning alert component manually

--- Max/max/src/app/app.module.ts
+++ Max/max/src/app/app.module.ts
@@ -3,11 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { TawiComponent } from './tawi/tawi.component';
+import { KokoComponent } from './koko/koko.component';
+import { WarningAlert } from './warning-alert/warning-alert.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TawiComponent
+    TawiComponent,
+    KokoComponent,
+    WarningAlert
   ],
   imports: [
     BrowserModule
