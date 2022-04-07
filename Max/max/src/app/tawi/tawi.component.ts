f730505e951716e0c6e34f9bdd7a21f0775eea30
John Muiruri <themasalacollective@gmail.com>
2022-04-07 08:39:35 +0300

    First component created called Tawi

---
+++ Max/max/src/app/tawi/tawi.component.ts
@@ -0,0 +1,13 @@
+import { Component } from "@angular/core";
+
+@Component({
+        selector: 'app-tawi',
+        templateUrl: './tawi.component.html', 
+        styleUrls: ['./tawi.component.css'], 
+        
+    }
+)
+
+export class TawiComponent{
+    // title: 'Tawi';
+}
