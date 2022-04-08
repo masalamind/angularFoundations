23176782af27d851c64e7902066e42a8afda904b
John Muiruri <themasalacollective@gmail.com>
2022-04-08 09:18:53 +0300

    Added more details on the template

--- Max/max/src/app/tawi/tawi.component.ts
+++ Max/max/src/app/tawi/tawi.component.ts
@@ -11,4 +11,11 @@ import { Component } from "@angular/core";
 export class TawiComponent{
     // title: 'Tawi';
     desc: string = "Tawi is an amazing product";  // use string interpolation to display it in template 
+
+    song: any = {
+        title: "Where I'll Always Be",
+        genre: "Indie Folk",
+        album: "Where I'll always be",
+        songNumber: 3
+    };
 }
