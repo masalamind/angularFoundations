1b52ff025ff38cf1e1f04f12b6fc7a73effca7ca
John Muiruri <themasalacollective@gmail.com>
2022-04-08 10:51:03 +0300

    Introduced a class component and used conditional directive ngIf

--- Goals/src/app/app.component.ts
+++ Goals/src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import { Goal } from './goal';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  goal = 'Watch Finding Nemo';
+
+  // goals: string[];
+  goals: Goal[] = [
+    {id:1, name: 'Watch finding nemo'},
+    {id:2, name: 'Buy cookies'},
+    {id:3, name: 'Get new phone case'},
+    {id:4, name: 'Get Dog food'},
+  ];
+
+  // constructor(){
+  //   this.goals = ['Watch Finding Nemo', 'Buy cookies', 'Get new phone case']
+  // }
+   
 }
---
+++ Goals/src/app/goal.ts
@@ -0,0 +1,10 @@
+export class Goal {
+    id: number;
+    name: string; 
+
+
+    constructor(){
+        this.id = -1;
+        this.name = "";
+    }
+}
