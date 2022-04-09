d4b51a3072403616a9f6ff77ad50c023e2f8cbfe
John Muiruri <themasalacollective@gmail.com>
2022-04-09 03:24:05 +0300

    Nested component to IntroToAngular directory

--- EnemyOfSyntax/src/app/app.module.ts
+++ EnemyOfSyntax/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { coolComponent } from './my-component/my-component.component';
+import { coolComponent } from './IntroToAngular/my-component/my-component.component';
 
 @NgModule({
   declarations: [
