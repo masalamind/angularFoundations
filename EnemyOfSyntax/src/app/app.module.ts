73e7b437fca7b59065f2c1aaec61680634c41e03
John Muiruri <themasalacollective@gmail.com>
2022-04-09 11:26:59 +0300

    Two way data binding awesomeness

--- EnemyOfSyntax/src/app/app.module.ts
+++ EnemyOfSyntax/src/app/app.module.ts
@@ -1,18 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { coolComponent } from './IntroToAngular/my-component/my-component.component';
 import { InterpolationComponent } from './Basics/DataBinding/interpolation-component/interpolation-comp.component';
 import { PropertyBindingComponent } from './Basics/DataBinding/property-binding-component/property-binding-component.component';
-import { EventBindingComponent } from './Basics/DataBinding/event-binding/event-binding.component'
+import { EventBindingComponent } from './Basics/DataBinding/event-binding/event-binding.component';
+import { TwoWayDataBindingComponent } from './Basics/DataBinding/two-way-data-binding/two-way-data-binding.component'
 
 @NgModule({
   declarations: [
-    AppComponent, coolComponent, InterpolationComponent, PropertyBindingComponent, EventBindingComponent
+    AppComponent, coolComponent, InterpolationComponent, PropertyBindingComponent, EventBindingComponent, TwoWayDataBindingComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
