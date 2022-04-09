c532cd545e08436cda82c749233bd6b7ae84f8b8
John Muiruri <themasalacollective@gmail.com>
2022-04-09 03:14:29 +0300

    Created a custom component

---
+++ EnemyOfSyntax/src/app/app.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { coolComponent } from './my-component/my-component.component';
+
+@NgModule({
+  declarations: [
+    AppComponent, coolComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ EnemyOfSyntax/src/app/my-component/my-component.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'a-cool-component',
+    templateUrl: './my-component.component.html',
+    styleUrls: ['./my-component.component.css']
+})
+
+export class coolComponent{
+
+}
